d8d0df5394f3edd52dd4b4c52ef907185f6bca7b
kakaemma <7983999+kakaemma@users.noreply.github.com>
2022-08-27 00:45:15 +0400

    update amounts in factory (#2)

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :transaction do
     identifier { Faker::Name.name }
     customer_id { Faker::Internet.uuid  }
-    input_amount { Faker::Internet.free_email }
+    input_amount { Faker::Number.number(digits: 4) }
     input_currency { Faker::Currency.code }
-    output_amount { Faker::Internet.password(11) }
+    output_amount { Faker::Number.number(digits: 4) }
     output_currency { Faker::Currency.code }
     transaction_date { rand(3).days.ago.to_date }
   end
